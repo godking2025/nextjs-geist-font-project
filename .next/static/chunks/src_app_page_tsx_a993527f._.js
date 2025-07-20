54e2e29dba0cdb7d0648651a3ccb6d01be010825
godking2025 <mindset.rulelikeaking@gmail.com>
2025-07-20 15:01:50 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753003908474

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_lib_ai_ts_474d84d4._.js",
+  "static/chunks/_32c13634._.js"
+],
+    source: "dynamic"
+});
